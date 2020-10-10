cc079dc46330eb9c2068121e64d6a20c51abca1d
AJ Larson <ajlarson@AJsMac.local>
2020-10-10 15:14:48 -0600

    Updates

---
+++ precache-manifest.c0a83a1736f3b3ac25eb2bca4f44590c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d94037122605ed99002",
+    "url": "/qa_recipes/static/css/main.92ad4a9b.chunk.css"
+  },
+  {
+    "revision": "5d94037122605ed99002",
+    "url": "/qa_recipes/static/js/main.5d940371.chunk.js"
+  },
+  {
+    "revision": "90c43adc59a5eb5734b0",
+    "url": "/qa_recipes/static/js/1.90c43adc.chunk.js"
+  },
+  {
+    "revision": "3fe1fb52e3bc66243670",
+    "url": "/qa_recipes/static/js/runtime~main.3fe1fb52.js"
+  },
+  {
+    "revision": "984c466fd1195ff0353dec24f5eabb3e",
+    "url": "/qa_recipes/index.html"
+  }
+];
